843aaf0e18f3e62f460ecf32336e691f284c0689
Unknown <97carreiraalbert@gmail.com>
2018-04-14 13:54:05 +0200

    SA Succ Func

--- src/iapractica1/IAPractica1.java
+++ src/iapractica1/IAPractica1.java
@@ -12,6 +12,7 @@ import aima.search.informed.SimulatedAnnealingSearch;
 import iapractica1.IAPractica1Board;
 import iapractica1.IAPractica1GoalTest;
 import iapractica1.IAPractica1HeuristicFunction;
+import iapractica1.IAPractica1SASuccesorFunction;
 import iapractica1.IAPractica1SuccesorFunction;
 import java.util.Iterator;
 import java.util.List;
@@ -21,23 +22,32 @@ import java.util.Random;
 public class IAPractica1 {   
     public static void main(String[] args) throws Exception {
         int ncentros = 5;
-        int ngrupos = 100;
+        int ngrupos = 20;
         int nhelicopteros = 1; // Numero de helicopteros en cada centro ¿? DUDA DOC.
         int initialState = 1;
-        int experiment = 1; // Heurístico tiempo total(1) o minimizando grupos prioridad 1 (2)
+        int heuristic = 1; // Heurístico tiempo total(1) o minimizando grupos prioridad 1 (2)
         
-        /*  SEEDS ALEATORIAS 
+        // Algoritmo: HC o SA
+        int steps = 800;
+        int stiter = 100;
+        int k = 3; 
+        double lamb = 3.5;
+        //Search alg = new HillClimbingSearch();
+        Search alg = new SimulatedAnnealingSearch(steps, stiter, k, lamb); //<-- No funciona con el printActions
+        
+        
+        //  SEEDS ALEATORIAS 
         int min = 0, max = 1000, rango = max - min +1;
         Random rand = new Random();
         int seedCentros = rand.nextInt(rango) + min;
         int seedGrupos = rand.nextInt(rango) + min;
-        */
-        int seedCentros = 1234, seedGrupos = 1234;
+        
+        //int seedCentros = 1234, seedGrupos = 1234;
         
         Centros cs = new Centros(ncentros, nhelicopteros, seedCentros);
         Grupos gs = new Grupos(ngrupos,seedGrupos);
           
-        IAPractica1Board board = new IAPractica1Board(initialState, nhelicopteros*ncentros, cs, gs, experiment);
+        IAPractica1Board board = new IAPractica1Board(initialState, nhelicopteros*ncentros, cs, gs, heuristic);
         
         
         // TEST CLONE 
@@ -56,19 +66,18 @@ public class IAPractica1 {
         */
 
         // Create the Problem object
-        Problem p = new  Problem(board,
-                                new IAPractica1SuccesorFunction(),
-                                new IAPractica1GoalTest(),
-                                new IAPractica1HeuristicFunction());
-
-        // Instantiate the search algorithm
-	// HillClimbingSearch() or SimulatedAnnealing(int steps, int stiter, int k, double lamb)        
-        int steps = 800;
-        int stiter = 100;
-        int k = 3; 
-        double lamb = 3.5;
-        Search alg = new HillClimbingSearch();
-        //Search alg = new SimulatedAnnealingSearch(steps, stiter, k, lamb); //<-- No funciona con el printActions
+        Problem p ;
+        if (alg.toString().contains("HillClimbing"))
+            p = new  Problem(board,
+                             new IAPractica1SuccesorFunction(),
+                             new IAPractica1GoalTest(),
+                             new IAPractica1HeuristicFunction());
+        
+        else //if (alg.toString().contains("SimulatedAnnealing"))    
+            p = new  Problem(board,
+                             new IAPractica1SASuccesorFunction(),
+                             new IAPractica1GoalTest(),
+                             new IAPractica1HeuristicFunction()); 
         
         // Instantiate the SearchAgent object
         long timeIni = java.lang.System.currentTimeMillis();
